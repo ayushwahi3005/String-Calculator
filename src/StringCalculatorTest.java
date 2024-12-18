b59cbe735514f62f96c99ea8cae6623d18ac47f8
Ayush <ayushwahi3005@gmail.com>
2024-12-19 03:45:54 +0530

    first commit

---
+++ src/StringCalculatorTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
+    }
+
+    @Test
+    public void testSingleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(1, calculator.add("1"));
+    }
+}
