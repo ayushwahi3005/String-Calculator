a26e86450081af531ddce82d67ab1aa4fbd11d0e
Ayush <ayushwahi3005@gmail.com>
2024-12-19 11:49:08 +0530

    removed extra code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        StringCalculator stringCalculator=new StringCalculator();
-        System.out.println(stringCalculator.add("1"));
     }
 }
